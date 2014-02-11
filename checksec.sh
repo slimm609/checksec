57cb2592c30b9f90457be8fad379b34691a8b08c
root <root@virus.slimm609.com>
2014-02-11 08:46:20 -0500

    cleaned up --proc output and reformatted ASLR output for --proc-*

--- checksec.sh
+++ checksec.sh
@@ -326,18 +326,18 @@ aslrcheck() {
     # (see the kernel file 'Documentation/sysctl/kernel.txt' for a detailed description)
     echo_message " (kernel.randomize_va_space): " '' '' ''
     if /sbin/sysctl -a 2>/dev/null | grep -q 'kernel.randomize_va_space = 1'; then
-      echo_message '\033[33mOn (Setting: 1)\033[m\n\n' '' '' ''
+      echo_message '\033[33mPartial (Setting: 1)\033[m\n\n' '' '' ''
       echo_message "  Description - Make the addresses of mmap base, stack and VDSO page randomized.\n" '' '' ''
       echo_message "  This, among other things, implies that shared libraries will be loaded to \n" '' '' ''
       echo_message "  random addresses. Also for PIE-linked binaries, the location of code start\n" '' '' ''
       echo_message "  is randomized. Heap addresses are *not* randomized.\n\n" '' '' ''
     elif /sbin/sysctl -a 2>/dev/null | grep -q 'kernel.randomize_va_space = 2'; then
-      echo_message '\033[32mOn (Setting: 2)\033[m\n\n' '' '' ''
+      echo_message '\033[32mFull (Setting: 2)\033[m\n\n' '' '' ''
       echo_message "  Description - Make the addresses of mmap base, heap, stack and VDSO page randomized.\n" '' '' ''
       echo_message "  This, among other things, implies that shared libraries will be loaded to random \n" '' '' ''
       echo_message "  addresses. Also for PIE-linked binaries, the location of code start is randomized.\n\n" '' '' ''
     elif /sbin/sysctl -a 2>/dev/null | grep -q 'kernel.randomize_va_space = 0'; then
-      echo_message '\033[31mOff (Setting: 0)\033[m\n' '' '' ''
+      echo_message '\033[31mNone (Setting: 0)\033[m\n' '' '' ''
     else
       echo_message '\033[31mNot supported\033[m\n' '' '' ''
     fi
@@ -902,7 +902,7 @@ do
     aslrcheck
     echo_message "* Does the CPU support NX: " '' '' ''
     nxcheck
-    echo_message "         COMMAND    PID RELRO             STACK CANARY           NX/PaX        PIE              FORTIFY\n" '' '' '{'
+    echo_message "         COMMAND    PID RELRO           STACK CANARY      NX/PaX        PIE 			FORTIFY\n" "" "" '{'
     for N in `ps -Ao pid,comm | grep $2 | cut -b1-6`; do
       if [ -d $N ] ; then
 	name=`head -1 $N/status | cut -b 7-`
