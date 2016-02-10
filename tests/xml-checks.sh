1ca1d9b57ed96824fbe37a3d499f07b3c02237b3
slimm609 <slimm609@gmail.com>
2016-02-10 00:22:19 -0500

    started validation tests for checksec

---
+++ tests/xml-checks.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+../checksec --help > /dev/null
+
+
+#check xml for proc-all
+../checksec --format xml --proc-all > output.xml
+xmllint --noout output.xml
+if [ $? != 0 ]; then
+ echo "proc-all xml validation failed"
+ #exit
+fi
+#check xml for kernel
+../checksec --format xml --kernel > output.xml
+xmllint --noout output.xml
+if [ $? != 0 ]; then
+ echo "kernel xml validation failed"
+ exit
+fi
+
+#check xml against custom kernel config to trigger all checks
+../checksec --format xml --kernel kernel.config > output.xml
+xmllint --noout output.xml
+if [ $? != 0 ]; then
+ echo "custom kernel xml validation failed"
+ exit
+fi
+#check xml for file
+../checksec --format xml --file /bin/ls > output.xml
+xmllint --noout output.xml
+if [ $? != 0 ]; then
+ echo "file xml validation failed"
+ exit
+fi
+
+
+
+
+rm -f output.xml
