106e19cf8c5630db5f922508de39175bd1873f52
Brian Davis <brian.davis@stopso.com>
2016-02-13 01:28:39 -0500

    added test and json

---
+++ tests/json-checks.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#check json for proc-all
+../checksec --format json --proc-all > output.json
+jsonlint --noout output.json
+if [ $? != 0 ]; then
+ echo "proc-all json validation failed"
+ exit
+fi
+#check json for kernel
+../checksec --format json --kernel > output.json
+jsonlint --noout output.json
+if [ $? != 0 ]; then
+ echo "kernel json validation failed"
+ exit
+fi
+
+#check json against custom kernel config to trigger all checks
+../checksec --format json --kernel kernel.config > output.json
+jsonlint --noout output.json
+if [ $? != 0 ]; then
+ echo "custom kernel json validation failed"
+ exit
+fi
+
+#check json for file
+../checksec --format json --file /bin/ls > output.json
+jsonlint --noout output.json
+if [ $? != 0 ]; then
+ echo "file json validation failed"
+ exit
+fi
+
+#check json for fortify file
+../checksec --format json --fortify-file /bin/ls > output.json
+jsonlint --noout output.json
+if [ $? != 0 ]; then
+ echo "fortify-file json validation failed"
+ exit
+fi
+ 
+#check json for dir 
+../checksec --format json --dir /sbin > output.json
+jsonlint --noout output.json
+if [ $? != 0 ]; then
+ echo "dir json validation failed"
+ exit
+fi
+
+
+
+echo "All XML validation tests passed jsonlint"
+rm -f output.json
---
+++ tests/test-checksec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# run a quick test of checksec to ensure normal operations. 
+
+
+./xml-checks.sh
+./json-checks.sh
