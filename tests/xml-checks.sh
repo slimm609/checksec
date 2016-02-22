2e59a5eac6245136ce08e282eac9997c4ca29da3
Brian Davis <brian.davis@stopso.com>
2016-02-22 10:59:54 -0500

    updated tests to pass on error codes from lint failures  #28

--- tests/xml-checks.sh
+++ tests/xml-checks.sh
@@ -4,54 +4,69 @@
 echo "starting proc-all check - xml"
 ../checksec --format xml --proc-all > output.xml
 xmllint --noout output.xml
-if [ $? != 0 ]; then
+RET=$?
+if [ $RET != 0 ]; then
  echo "proc-all xml validation failed"
- exit
+ exit $RET
 fi
 
 #check xml for kernel
 echo "starting kernel check - xml"
 ../checksec --format xml --kernel > output.xml
 xmllint --noout output.xml
-if [ $? != 0 ]; then
+RET=$?
+if [ $RET != 0 ]; then
  echo "kernel xml validation failed"
- exit
+ exit $RET
 fi
 
 #check xml against custom kernel config to trigger all checks
 echo "starting custom kernel check - xml"
 ../checksec --format xml --kernel kernel.config > output.xml
 xmllint --noout output.xml
-if [ $? != 0 ]; then
+RET=$?
+if [ $RET != 0 ]; then
  echo "custom kernel xml validation failed"
- exit
+ exit $RET
 fi
 
 #check xml for file
 echo "starting file check - xml"
 ../checksec --format xml --file /bin/ls > output.xml
 xmllint --noout output.xml
-if [ $? != 0 ]; then
+RET=$?
+if [ $RET != 0 ]; then
  echo "file xml validation failed"
- exit
+ exit $RET
 fi
 
 #check xml for fortify file
 echo "starting fortify-file check - xml"
-../checksec --format xml --fortify-file /bin/ls > output.xml
+if [ -f /bin/ls ]; then 
+../checksec --format xml --fortify-file /bin/ls > output.json
+elif [ -f /bin/bash ]; then
+../checksec --format xml --fortify-file /bin/bash > output.json
+elif [ -f /bin/sh ]; then
+../checksec --format xml --fortify-file /bin/sh > output.json
+else
+ echo "could not find valid file to test"
+ exit 255
+fi
 xmllint --noout output.xml
-if [ $? != 0 ]; then
+RET=$?
+if [ $RET != 0 ]; then
  echo "fortify-file xml validation failed"
- exit
+ exit $RET
 fi
  
 #check xml for dir 
 echo "starting dir check - xml"
 ../checksec --format xml --dir /sbin > output.xml
 xmllint --noout output.xml
-if [ $? != 0 ]; then
+RET=$?
+if [ $RET != 0 ]; then
  echo "dir xml validation failed"
- exit
+ exit $RET
 fi
 
 
