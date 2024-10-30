6428a63f1781d752429692db7a49bd8c734b0f4f
Brian Davis <slimm609@gmail.com>
2024-10-29 20:56:01 -0400

    chore: minor updates and cleanup
    - add version number to cli
    - remove zsh tab completion

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -15,6 +16,10 @@ var rootCmd = &cobra.Command{
 	Use:   "checksec",
 	Short: "A binary scanning security tool",
 	Long:  `A tool used to quickly survey mitigation technologies in use by processes on a Linux system.`,
+}
+
+func SetVersionInfo(version, commit, date string) {
+	rootCmd.Version = fmt.Sprintf("%s (Built on %s from Git SHA %s)", version, date, commit)
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "github.com/slimm609/checksec/cmd"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
