56443da9cea98dfd0bd8f8593b12463b3b438575
Maosu Zhao <maosu.zhao@intel.com>
2025-11-13 14:17:55 +0800

    feat(relro): detect BIND_NOW via DT_FLAGS_1 (DF_1_NOW) and use bitmask for DF_BIND_NOW
    Add support for detecting RELRO/BIND_NOW through DT_FLAGS_1 (DF_1_NOW),
    in addition to DT_BIND_NOW and DT_FLAGS. Also switch DF_BIND_NOW check
    from equality (8) to a proper bitmask, and rename variables to Go-style
    camelCase for clarity.

--- pkg/checksec/relro.go
+++ pkg/checksec/relro.go
@@ -34,17 +34,25 @@ func RELRO(name string) *relro {
 	// check both bind and bind_flag.
 	// if DT_BIND_NOW == 0, then it is set
 	// if DT_FLAGS == 8, then DF_BIND_NOW is set
+	// if DT_FLAGS_1 == 1, then DF_1_NOW is set
 	// this is depending on the compiler version used.
 	bind, _ := file.DynValue(elf.DT_BIND_NOW)
 	if len(bind) == 0 {
 		bind, _ = DynValueFromPTDynamic(file, elf.DT_BIND_NOW)
 	}
-	bind_flag, _ := file.DynValue(elf.DT_FLAGS)
-	if len(bind_flag) == 0 {
-		bind_flag, _ = DynValueFromPTDynamic(file, elf.DT_FLAGS)
+	bindFlag, _ := file.DynValue(elf.DT_FLAGS)
+	if len(bindFlag) == 0 {
+		bindFlag, _ = DynValueFromPTDynamic(file, elf.DT_FLAGS)
 	}
 
-	if (len(bind) > 0 && bind[0] == 0) || (len(bind_flag) > 0 && bind_flag[0] == 8) {
+	bindFlag1, _ := file.DynValue(elf.DT_FLAGS_1)
+	if len(bindFlag1) == 0 {
+		bindFlag1, _ = DynValueFromPTDynamic(file, elf.DT_FLAGS_1)
+	}
+
+	if (len(bind) > 0 && bind[0] == 0) ||
+		(len(bindFlag) > 0 && (bindFlag[0]&uint64(elf.DF_BIND_NOW)) != 0) ||
+		(len(bindFlag1) > 0 && (bindFlag1[0]&uint64(elf.DF_1_NOW)) != 0) {
 		bindNow = true
 	}
 
